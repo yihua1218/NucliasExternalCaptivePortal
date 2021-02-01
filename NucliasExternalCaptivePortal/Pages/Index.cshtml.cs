9a91d70ec4c610dca2f0e520f21ddc119399150a
Yi-Hua Liang <yihua1218@gmail.com>
2021-02-01 18:42:20 +0800

    Initial version.

--- NucliasExternalCaptivePortal/Pages/Index.cshtml.cs
+++ NucliasExternalCaptivePortal/Pages/Index.cshtml.cs
@@ -1,16 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NucliasExternalCaptivePortal.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public StringValues ContinueUrl;
+        public StringValues LoginUrl;
+        public StringValues LogoutUrl;
+        public StringValues ApName;
+        public StringValues ApTags;
+        public StringValues ApMac;
+        public StringValues Mauth;
+        public StringValues LoginResult;
+        public StringValues RadiusUserName;
+        public StringValues RadiusPassword;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +27,26 @@ namespace NucliasExternalCaptivePortal.Pages
 
         public void OnGet()
         {
-
+            ContinueUrl = Request.Query["continue_url"];
+            System.Console.WriteLine($"ContinueUrl is {ContinueUrl}");
+            LoginUrl = Request.Query["login_url"];
+            System.Console.WriteLine($"LoginUrl is {LoginUrl}");
+            LogoutUrl = Request.Query["logout_url"];
+            System.Console.WriteLine($"LogoutUrl is {LogoutUrl}");
+            ApName = Request.Query["ap_name"];
+            System.Console.WriteLine($"ApName is {ApName}");
+            ApTags = Request.Query["ap_tags"];
+            System.Console.WriteLine($"ApTags is {ApTags}");
+            ApMac = Request.Query["ap_mac"];
+            System.Console.WriteLine($"ApMac is {ApMac}");
+            Mauth = Request.Query["mauth"];
+            System.Console.WriteLine($"Mauth is {Mauth}");
+            LoginResult = "";
+        }
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            // perform action
+            LoginResult = "Success";
         }
     }
 }
--- NucliasExternalCaptivePortal/Startup.cs
+++ NucliasExternalCaptivePortal/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace NucliasExternalCaptivePortal
 {
@@ -16,6 +19,7 @@ namespace NucliasExternalCaptivePortal
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            System.Console.WriteLine($"RuntimeInformation.FrameworkDescription: {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}");
         }
 
         public IConfiguration Configuration { get; }
@@ -50,6 +54,28 @@ namespace NucliasExternalCaptivePortal
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapPost("api/login", async context =>
+                {
+                    var form = await context.Request.ReadFormAsync();
+                    var userName = form["UserName"][0];
+                    System.Console.WriteLine($"userName is {userName}");
+                    var password = form["Password"][0];
+                    System.Console.WriteLine($"password is {password}");
+                    var loginUrl = form["login_url"][0];
+                    System.Console.WriteLine($"loginUrl is {loginUrl}");
+                    var successUrl = form["success_url"][0];
+                    System.Console.WriteLine($"successUrl is {successUrl}");
+
+                    context.Response.StatusCode = StatusCodes.Status200OK;
+   
+                    await context.Response.WriteAsJsonAsync(new {
+                        Status = "OK",
+                        RadiusUserName = "radius_user1",
+                        RadiusPassword = "radius_password",
+                        LoginUrl = loginUrl,
+                        SuccessUrl = successUrl,
+                    });
+                });
             });
         }
     }
